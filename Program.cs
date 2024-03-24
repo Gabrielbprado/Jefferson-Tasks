d6b7cbf76f12cd15b213c120a01619728c571e83
Gabriel <126411934+Gabrielbprado@users.noreply.github.com>
2024-03-24 11:36:55 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main()
     {
+        /// <summary>
+    /// <author>Gabriel Prado</author>
+    /// </summary>
         Car car = new Car();
         AirPlane airplane = new AirPlane();
         Console.WriteLine("Hello");
